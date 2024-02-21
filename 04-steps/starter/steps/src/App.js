283cdffb47c4bd2a53155e3dfba131506a53c6d2
glauuucoma <kulebadavid@gmail.com>
2024-02-21 02:24:17 -0500

    complete section

---
+++ 04-steps/starter/steps/src/App.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children pop</p>
+        <p>😎</p>
+      </StepMessage>
+      <StepMessage step={3}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((open) => !open)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          <div className="buttons">
+            <Button
+              color="fff"
+              backColor="#7950f2"
+              handleEvent={handlePrevious}
+            >
+              <span>👈</span> Previous
+            </Button>
+            <Button color="fff" backColor="#7950f2" handleEvent={handleNext}>
+              Next <span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </p>
+  );
+}
+
+function Button({ backColor, color, handleEvent, children }) {
+  return (
+    <button
+      style={{ backgroundColor: { backColor }, color: { color } }}
+      onClick={handleEvent}
+    >
+      {children}
+    </button>
+  );
+}
